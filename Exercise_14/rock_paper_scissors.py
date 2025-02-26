ecff0924901d9da5ea96aa6ddd36d4da44959269
chaitra <chaitrachubz456@gmail.com>
2025-02-26 12:57:12 +0000

    Updated with new comments

--- Exercise_14/rock_paper_scissors.py
+++ Exercise_14/rock_paper_scissors.py
@@ -1,16 +1,18 @@
-import random
+import random     # Import the random module to generate random numbers
 
+# defining the function
 def get_user_choice() :
     """ This function prompts the user to enter their choice of Rock, Paper, or Scissors.
     :return: str Represents the user's choice as a string, either "Rock", "Paper", or "Scissors"."""
     choices = {'R': 'Rock', 'P': 'Paper', 'S': 'Scissors'}  # Dictionary to map user input
-    user_input = input("Enter R for Rock, P for Paper, or S for Scissors:\n").upper()
+    user_input = input("Please Enter your Choice(R,P or S) R for Rock, P for Paper, or S for Scissors:\n").upper()
     if user_input in choices:      # user_input = 'R'
         user_response = choices[user_input]  # choices[R] is 'Rock', because 'R' maps to 'Rock' in choices.
         return user_response      # This returns the valid choice, e.g., "Rock"
     else:
-        print("Invalid input! Please enter R,P or S.")
+        print("Invalid choice! Please enter R,P or S.")
         return get_user_choice()      # This calls the function again to ask the user for input again
+    # The return statement returns an object of the type based on the expression's
 
 def get_computer_choice():
     """This function randomly selects a choice for the computer: Rock, Paper, or Scissors.
@@ -18,12 +20,12 @@ def get_computer_choice():
     choices = {0: 'Rock', 1: 'Paper', 2: 'Scissors'}   # Dictionary to map numbers to choices
     random_number = random.randint(0, 2)  # Generate a random number between 0 and 2
     comp_response = choices[random_number]
-    return comp_response   # Return the corresponding choice
+    return comp_response   # Return the value back to the user
 
 def determine_winner(user, computer):
     """This function compares the user's and computer's choices to determine the winner.
-     :param user: str Represents the user's choice ("Rock", "Paper", or "Scissors").
-     :param computer: str Represents the computer's choice ("Rock", "Paper", or "Scissors").
+     :param1 user: str Represents the user's choice ("Rock", "Paper", or "Scissors").
+     :param2 computer: str Represents the computer's choice ("Rock", "Paper", or "Scissors").
      :return: str The result of the game ("You win!", "You lose!", or "It's a draw!")."""
     if user == computer:        # If both choices are the same, it's a draw
         return "It's a draw!"
@@ -36,7 +38,7 @@ def determine_winner(user, computer):
         ):
         return "YOU WIN!!"     # User wins
     else:
-        return "You lose!"
+        return "YOU LOSE!"
 
 
 def play_game():
@@ -44,17 +46,22 @@ def play_game():
     """The main entry point of the program where the game logic is executed.
     Plays a complete game of Rock, Paper, Scissors between the user and the computer.
     :return: None This function does not return any value."""
-    print("Welcome to Rock, Paper, Scissors! Let's Start the Game.")
+    print("*" * 80)
+    print("~~~ Welcome to the Game of Rock, Paper, Scissors! Let's Start the Game. ~~~")
     user_choice= get_user_choice()
     comp_choice = get_computer_choice()
 
     print(f"\nThe user choice is : {user_choice}")      # Display user choice
     print(f"The Computer choice is : {comp_choice}")  # Display computer choice
+    print("~" * 50)
 
     result = determine_winner(user_choice,comp_choice)  # Determine the winner
-    print(result)
+    print(f"The result is : {result} ")
+    print("~" * 50)
 
 # This block will only run when rock_paper_scissors.py is executed directly,not when imported.
 # It prevents the game logic from running automatically when the script is imported into another program.
 if __name__ == "__main__":
    play_game()
+
+
--- Exercise_14/use_rps.py
+++ Exercise_14/use_rps.py
@@ -1,12 +1,21 @@
 import rock_paper_scissors
+from Exercise_14.rock_paper_scissors import play_game
 
-# Now we can use the functions directly without triggering play_game(),just call the function from the script name
-u_response = rock_paper_scissors.get_user_choice()
-print(f"The user choice is {u_response}")
+# print("This is the 'using' file")
+# # Now we can use the functions directly without triggering play_game(),just call the function from the script name
+# u_response = rock_paper_scissors.get_user_choice()
+# print(f"The user choice is {u_response}")
+#
+# c_response = rock_paper_scissors.get_computer_choice()
+# print(f"The computer choice is {c_response}")
+#
+# result = rock_paper_scissors.determine_winner(u_response,c_response)
+# print(result)
 
-c_response = rock_paper_scissors.get_computer_choice()
-print(f"The computer choice is {c_response}")
+play_game()
 
-result = rock_paper_scissors.determine_winner(u_response,c_response)
-print(result)
+#help(rock_paper_scissors)
+
+# functionality_list = dir(rock_paper_scissors)
+# print(functionality_list)
 
